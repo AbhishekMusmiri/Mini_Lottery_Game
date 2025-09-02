99b0f87bd4539809ee6e8c741c269ccd578de79f
Abhishek Musmiri <abhishekmusmiri07@gmail.com>
2025-09-02 17:04:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let playbtn = document.getElementById("playbtn");
+let result = document.getElementById("display_result");
+let result_num = document.getElementById("result_num");
+let clickSound = new Audio("./audio/button-305770.mp3");
+
+let gifts = ["laptop", "₹ 25,000", "Mobile Phone", "Cycle", "Scooty", "Car"];
+playbtn.addEventListener("click", function() {
+    clickSound.currentTime = 0; 
+    clickSound.play();
+
+    let dice = Math.round(Math.random() * 5) + 1;
+    result_num.textContent = dice;
+    result.innerHTML = `You rolled  ${dice} , Hurray!! You won <span class="span_gift"> ${gifts[dice - 1]}</span>`;
+
+});
+    console.log(Math.floor((Math.random()*6)+1));
